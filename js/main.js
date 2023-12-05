66a52f0820a8d4307dc234031e61b42bdebe213c
mohamedzrn <mohammedzrn13@gmail.com>
2023-12-05 01:14:29 -0500

    homepage content fit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// nav-menu
+(function(){
+    "use strict";
+
+    console.log("fired");
+
+    let button = document.querySelector("#hamburger");
+    let burgerCon = document.querySelector("#burger-con");
+
+    function hamburgerMenu() {
+        burgerCon.classList.toggle("slide-toggle");
+        button.classList.toggle("expanded");
+    };
+
+    button.addEventListener("click", hamburgerMenu, false);
+
+})();
